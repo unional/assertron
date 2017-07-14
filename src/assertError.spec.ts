8ebb2b34ee70561abdffe07962506193de0026e2
kvickyk <kvickyk@gmail.com>
2017-07-13 18:16:30 -0700

    Added AssertError class for handling assertion errors
    AssertError extends the Error class and takes in the state of the
    assertOrder instance to create a standard error message. The unit test
    ensures that the error message is being generated in the right format.

---
+++ src/assertError.spec.ts
@@ -0,0 +1,20 @@
+﻿import test from 'ava'
+import AssertError from './assertError'
+
+test('test error format based on input', _t => {
+  let reverseAlias = {
+    alias1: ['method1', 'method2'],
+    alias2: [], // new method
+    alias3: ['method3']
+  }
+
+  let possibleMoves = {
+    alias1: [2],
+    alias2: [2],
+    alias3: [1, 2]
+  }
+
+  let error = new AssertError(possibleMoves, reverseAlias, 'calledMethod', 3, 4, 5);
+  console.log(error.message);
+  _t.is(error.message, 'Expecting \'alias1(2)\', \'method1(2)\', \'method2(2)\', \'alias2(2)\', \'alias3(1|2)\', \'method3(1|2)\', but received \'calledMethod(3,4,5)\'')
+})
