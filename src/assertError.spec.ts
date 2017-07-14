e2ced75b9f16014e0cbf1de72337448434957452
kvickyk <kvickyk@gmail.com>
2017-07-13 21:20:06 -0700

    code review feedback

--- src/assertError.spec.ts
+++ src/assertError.spec.ts
@@ -1,7 +1,7 @@
 import test from 'ava'
 import { AssertError } from './assertError'
 
-test('test error format based on input', _t => {
+test('test error format based on input', t => {
   let reverseAlias = {
     once: ['method1', 'method2'],
     some: [], // new method
@@ -15,5 +15,5 @@ test('test error format based on input', _t => {
   }
 
   let error = new AssertError(possibleMoves, reverseAlias, 'calledMethod', 3, 4, 5);
-  _t.is(error.message, "Expecting 'once(2)', 'method1(2)', 'method2(2)', 'some(2)', 'all(1|2)', 'method3(1|2)', but received 'calledMethod(3,4,5)'")
+  t.is(error.message, "Expecting 'once(2)', 'method1(2)', 'method2(2)', 'some(2)', 'all(1|2)', 'method3(1|2)', but received 'calledMethod(3,4,5)'")
 })
