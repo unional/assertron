bfb4cf9d03ca2e2241e8ee9a088061ede0a0355a
Homa Wong <homawong@gmail.com>
2017-10-28 14:25:54 -0700

    Update deps (#40)

--- scripts/setup-test-env.js
+++ scripts/setup-test-env.js
@@ -1,7 +1,7 @@
-require('aurelia-polyfills')
-const LogManager = require('aurelia-logging')
-const ColorAppender = require('aurelia-logging-color').ColorAppender
+// require('aurelia-polyfills')
+// const LogManager = require('aurelia-logging')
+// const ColorAppender = require('aurelia-logging-color').ColorAppender
 
-LogManager.addAppender(new ColorAppender())
+// LogManager.addAppender(new ColorAppender())
 
-LogManager.setLevel(LogManager.logLevel.debug)
+// LogManager.setLevel(LogManager.logLevel.debug)
