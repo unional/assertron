bfdf1884f1bc80b663c6140c57d821b7c46ff657
Homa Wong <homawong@gmail.com>
2017-10-29 23:30:17 -0700

    rename

--- src/AssertError.spec.ts
+++ src/AssertError.spec.ts
@@ -1,19 +1,61 @@
 import test from 'ava'
-import { AssertError } from './assertError'
+import { AssertOrder } from './index'
 
-test('test error format based on input', t => {
-  let reverseAlias = {
-    once: ['method1', 'method2'],
-    some: [], // new method
-    all: ['method3']
-  }
+test('error message for once()', t => {
+  const o = new AssertOrder()
+  const err = t.throws(() => o.once(2))
+  t.is(err.message, `Expecting 'is(1)', 'once(1)', 'any([1])', but received 'once(2)'`)
+})
 
-  let possibleMoves = {
-    once: [2],
-    some: [2],
-    all: [1, 2]
-  }
+test('error message for multi-step methods', t => {
+  const o = new AssertOrder()
+  o.exactly(1, 2)
+  const err = t.throws(() => o.once(2))
+  t.is(err.message, `Expecting 'exactly(1, 2)', but received 'once(2)'`)
+})
 
-  let error = new AssertError(possibleMoves, reverseAlias, 'calledMethod', 3, 4, 5);
-  t.is(error.message, "Expecting 'once(2)', 'method1(2)', 'method2(2)', 'some(2)', 'all(1|2)', 'method3(1|2)', but received 'calledMethod(3,4,5)'")
+test('error message for exactly() called too many times', t => {
+  const o = new AssertOrder()
+  o.exactly(1, 2)
+  o.exactly(1, 2)
+  const err = t.throws(() => o.exactly(1, 2))
+  t.is(err.message, `Expecting 'is(2)', 'once(2)', 'any([2])', but received 'exactly(1, 2)'`)
+})
+
+
+test('error message for is()', t => {
+  const order = new AssertOrder()
+  const err = t.throws(() => order.is(0))
+  t.is(err.message, `Expecting 'is(1)', 'once(1)', 'any([1])', but received 'is(0)'`)
+})
+
+
+test(`error message for not()`, t => {
+  const order = new AssertOrder()
+  const err = t.throws(() => order.not(1))
+  t.is(err.message, `Expecting 'is(1)', 'once(1)', 'any([1])', but received 'not(1)'`)
+})
+
+test('error message for atLeastOnce()', t => {
+  const order = new AssertOrder()
+  order.once(1)
+  const err = t.throws(() => order.atLeastOnce(1))
+  t.is(err.message, `Expecting 'is(2)', 'once(2)', 'any([2])', but received 'atLeastOnce(1)'`)
+})
+
+test('error message for any()', t => {
+  const order = new AssertOrder()
+
+  let err = t.throws(() => order.any([2]))
+  t.is(err.message, `Expecting 'is(1)', 'once(1)', 'any([1])', but received 'any([2])'`)
+
+  err = t.throws(() => order.any([2, 3]))
+  t.is(err.message, `Expecting 'is(1)', 'once(1)', 'any([1])', but received 'any([2, 3])'`)
+})
+
+test('error message for end()', t => {
+  const order = new AssertOrder(1)
+
+  const err = t.throws(() => order.end())
+  t.is(err.message, `Expecting 'is(1)', 'once(1)', 'any([1])', but received 'end()'`)
 })
