4a59c93899e304bce9c27dbbac158f1fcc3a35a0
unional <homawong@gmail.com>
2017-11-26 16:00:24 -0800

    fix: adjust satisfy() behavior
    see `satisfier` for how it should behave

--- src/satisfy.ts
+++ src/satisfy.ts
@@ -1,64 +1,40 @@
-export type SatisfyExpected<T> = {
-  [P in keyof T]: Partial<SatisfyExpected<T[P]>> | RegExp | Function
+import { createSatisfier, Struct, SatisfierExec, Expecter } from 'satisfier'
+import { tersify } from 'tersify'
+
+export class NotSatisfied extends Error {
+  constructor(public entries: SatisfierExec[]) {
+    super(format(entries).join('\n'))
+    Object.setPrototypeOf(this, new.target.prototype)
+  }
 }
 
+function format(entries: SatisfierExec[]) {
+  return entries.map(e => {
+    const path = formatPath(e.path)
+    return `Expect ${path} to satisfy ${tersify(e.expected)}, but received ${tersify(e.actual)}`
+  })
+}
+
+function formatPath(entries: string[]) {
+  if (entries.length === 0)
+    return 'actual'
+  const nodes = [...entries]
+  if (isIndice(nodes[0])) {
+    nodes.unshift('actual')
+  }
+  return nodes.map(n => isIndice(n) ? n : '.' + n).join('').slice(1)
+}
+
+function isIndice(node: string) {
+  return /\[\d*\]/.test(node)
+}
 /**
  * Check if `actual` satisfies criteria in `expected`.
  * @param expected All properties can be a value which will be compared to the same property in `actual`, RegExp, or a predicate function that will be used to check against the property.
  */
-export function satisfy<
-  Actual,
-  // Using `| any` to disable type checking due to:
-  // https://github.com/Microsoft/TypeScript/issues/20247
-  Expected extends Partial<SatisfyExpected<Actual>> | any>(actual: Actual, expected: Expected) {
-  satisfyInternal(actual, expected, '')
-}
-export class FailedPredicate extends Error {
-  constructor(path: string) {
-    super(`Property ${path} fails predicate`)
-    Object.setPrototypeOf(this, new.target.prototype)
+export function satisfy<Actual extends Struct>(actual: Actual, expected: Expecter<Partial<Actual>>) {
+  const diff = createSatisfier(expected).exec(actual);
+  if (diff) {
+    throw new NotSatisfied(diff)
   }
-}
-export class NotMatch extends Error {
-  constructor(actual, expected, path) {
-    super(`Expect ${path ? path : 'actual'} to be ${typeof expected === 'string' ? `'${expected}'` : expected}, but received ${typeof actual === 'string' ? `'${actual}'` : actual}`)
-    Object.setPrototypeOf(this, new.target.prototype)
-  }
-}
-export class MissingProperty extends Error {
-  constructor(path: string) {
-    super(`Missing property ${path}`)
-    Object.setPrototypeOf(this, new.target.prototype)
-  }
-}
-
-function satisfyInternal(actual, expected, path: string) {
-  if (expected instanceof RegExp && typeof actual === 'string')
-    expected.test(actual)
-  else if (expected instanceof Function) {
-    if (!expected(actual))
-      throw new FailedPredicate(path)
-  }
-  // tslint:disable-next-line
-  else if (typeof actual !== typeof expected)
-    throw new Error(`Type mismatch. Expecting ${typeof expected}`)
-  else if (Array.isArray(actual)) {
-    if (actual.length !== expected.length)
-      throw new Error(`Number of entries does not match`)
-    actual.forEach((a, i) => {
-      const e = expected[i]
-      satisfyInternal(a, e, `entry[${i}]`)
-    })
-  }
-  else if (typeof actual !== 'object') {
-    if (!Object.is(actual, expected))
-      throw new NotMatch(actual, expected, path)
-  }
-  else
-    Object.keys(expected).forEach(k => {
-      if (actual[k] === undefined)
-        throw new MissingProperty(path ? `${path}.${k}` : k)
-      else
-        satisfyInternal(actual[k], expected[k], path ? `${path}.${k}` : k)
-    })
 }
