36685695ee05aeb225db61a027d73dac1652c85c
unional <homawong@gmail.com>
2018-01-10 01:16:18 -0800

    chore: fix version check

--- src/StateMachine.ts
+++ src/StateMachine.ts
@@ -1,6 +1,18 @@
 import { State } from './interfaces'
 
-const performance = require.resolve('perf_hooks') ? require('perf_hooks').performance : undefined
+// istanbul ignore next
+function nodeVersionAbove(major: number, minor = 0, patch = 0) {
+  // without this, systemjs will complain `process is not defined`
+  if (!global.process)
+    return false
+  const versionString = process.version.startsWith('v') ? process.version.slice(1) : process.version
+  const [actualMajor, actualMinor, actualPatch] = versionString.split('.').map(s => parseInt(s, 10))
+  const checking = major * 1000 * 1000 + minor * 1000 + patch
+  const actual = actualMajor * 1000 * 1000 + actualMinor * 1000 + actualPatch
+  return actual >= checking
+}
+
+const performance = nodeVersionAbove(7) ? require('perf_hooks').performance : undefined
 
 let startTick
 let timeTaken
