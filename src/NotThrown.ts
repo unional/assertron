61d2231927b9d888558880feb8b4231ffd6edfe4
Homa Wong <homawong@gmail.com>
2018-05-06 18:49:24 -0700

    fix: improve NotThrown message

--- src/NotThrown.ts
+++ src/NotThrown.ts
@@ -3,7 +3,7 @@ import { BaseError } from 'make-error'
 export class NotThrown extends BaseError {
   // istanbul ignore next
   constructor(public value: any) {
-    super(`Expect function to throw, but it returned instead '${value}'`)
+    super(`Expect function to throw, but it returned '${value}' instead.`)
 
     Object.setPrototypeOf(this, new.target.prototype)
   }
