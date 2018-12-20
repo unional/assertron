2889ff710d489ed07446d860a2b320b0eb5703c4
Homa Wong <homawong@gmail.com>
2018-12-19 16:11:36 -0800

    chore: improve coverage (#116)

--- src/NotSatisfied.spec.ts
+++ src/NotSatisfied.spec.ts
@@ -13,3 +13,21 @@ test('array should be tersified', () => {
   }])
   t.strictEqual(err.toString(), `NotSatisfied: Expect a to satisfy { b: ['a', 'b', 'c'] }, but received { a: [1, 2, 3] }`)
 })
+
+test(`root diff is displayed as 'actual'`, () => {
+  const err = new NotSatisfied([{
+    path: [],
+    actual: 1,
+    expected: 2
+  }])
+  t.strictEqual(err.toString(), `NotSatisfied: Expect actual to satisfy 2, but received 1`)
+})
+
+test(`root diff is displayed as 'actual'`, () => {
+  const err = new NotSatisfied([{
+    path: ['[0]'],
+    actual: 1,
+    expected: 2
+  }])
+  t.strictEqual(err.toString(), `NotSatisfied: Expect actual[0] to satisfy 2, but received 1`)
+})
