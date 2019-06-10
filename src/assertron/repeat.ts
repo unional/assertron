f5bfffd217788f5860a10cd9f4713acc2ae069cc
Homa Wong (unional) <homawong@gmail.com>
2019-06-09 21:33:41 -0700

    fix: repeat() to throw FailOnOccurance error
    when one of the calls failed.

--- src/assertron/repeat.ts
+++ src/assertron/repeat.ts
@@ -1,20 +1,37 @@
 import isPromise from 'is-promise';
+import AssertionError from 'assertion-error'
 
 /**
  * Repeat the specified function n times and return the last result.
  * If the result is a promise, it will run the function sequentially.
  */
 export function repeat<R>(fn: () => R | (() => Promise<R>), times: number): ReturnType<typeof fn> extends Promise<R> ? Promise<R> : R {
-  const result = fn()
-  if (isPromise(result)) {
-    if (times <= 1) return result as any
-    return result.then(() => repeat(fn, times - 1)) as any
-  }
-  else {
-    let result
-    for (let i = 1; i < times; i++) {
-      result = fn()
+  return repeatRecur(fn, 1, times)
+}
+
+function repeatRecur<R>(fn: () => R | (() => Promise<R>), i: number, times: number): ReturnType<typeof fn> extends Promise<R> ? Promise<R> : R {
+  try {
+    const result = fn()
+    if (i === times) return result as any
+
+    if (isPromise(result)) {
+      return result.then(
+        () => repeatRecur(fn, i + 1, times),
+        (e) => { throw new FailOnOccurance(i, e) }
+      ) as any
     }
-    return result
+    else {
+      return repeatRecur(fn, i + 1, times)
+    }
+  }
+  catch (e) {
+    if (e instanceof FailOnOccurance) throw e
+    throw new FailOnOccurance(i, e)
+  }
+}
+
+export class FailOnOccurance extends AssertionError {
+  constructor(public occurance: number, public error: any) {
+    super(`Failed on ${occurance} occurance`)
   }
 }
