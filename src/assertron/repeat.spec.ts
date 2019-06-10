b355b63ac202f61caf7e6119e28faee03ab34b51
Homa Wong <homawong@gmail.com>
2019-06-09 22:21:23 -0700

    fix: repeat() to throw FailOnOccurance error (#127)
    when one of the calls failed.

--- src/assertron/repeat.spec.ts
+++ src/assertron/repeat.spec.ts
@@ -1,4 +1,5 @@
 import a from '..'
+import { FailOnOccurance } from './repeat';
 
 test('repeat function n times', () => {
   let count = 0
@@ -20,4 +21,29 @@ test('repeat async function n times sequentially', async () => {
 
   expect(actual).toBe('12345678910')
   expect(actual).toBe(aa)
+})
+
+test('error is captured as inner error and indicate which call failed', () => {
+  let count = 0
+  const err = a.throws(() => a.repeat(() => {
+    ++count
+    if (count === 5) throw new Error('failed')
+  }, 10), FailOnOccurance)
+
+  expect(err.occurance).toBe(5)
+  expect(err.error.message).toBe('failed')
+})
+
+test('rejected error is captured as inner error and indicate which call failed', async () => {
+  let count = 0
+  let err = await a.throws(a.repeat(() => new Promise((a, r) => {
+    setTimeout(() => {
+      ++count
+      if (count === 5) r(new Error('failed'))
+      else a()
+    }, Math.random() * 10)
+  }), 10), FailOnOccurance)
+
+  expect(err.occurance).toBe(5)
+  expect(err.error.message).toBe('failed')
 })
