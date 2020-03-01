9ee4c6a26a76fb727b449f9cf79170d2a13e45e3
Homa Wong <homawong@gmail.com>
2020-03-01 14:09:46 -0800

    fix: use isPrototypeOf from Object

--- src/errors/util.ts
+++ src/errors/util.ts
@@ -1,5 +1,5 @@
 import { ErrorConstructor } from './interfaces'
 
 export function isErrorConstructor(validator): validator is ErrorConstructor<any> {
-  return Error.prototype.isPrototypeOf(validator.prototype)
+  return Object.prototype.isPrototypeOf.call(Error.prototype, validator.prototype)
 }
