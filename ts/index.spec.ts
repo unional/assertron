a3ec66f5dd249321ce076e0d4287556fee966cae
unional <homawong@gmail.com>
2023-04-12 23:26:55 -0700

    test: add expose test

--- ts/index.spec.ts
+++ ts/index.spec.ts
@@ -1,8 +1,37 @@
-import type { SatisfyExpectation } from './index.js'
+import {
+	SatisfyExpectation,
+	anything,
+	every,
+	has,
+	hasAll,
+	isInClosedInterval,
+	isInLeftClosedInterval,
+	isInOpenInterval,
+	isInRange,
+	isInRightClosedInterval,
+	none,
+	some,
+	startsWith
+} from './index.js'
 
 it('expose SatifyExpression type', () => {
-  function toSatisfies(value: SatisfyExpectation<number>) {
-    return value
-  }
-  toSatisfies(1)
+	function toSatisfies(value: SatisfyExpectation<number>) {
+		return value
+	}
+	toSatisfies(1)
+})
+
+it('expose types from satisfier', () => {
+	expect(anything).toBeDefined()
+	expect(has).toBeDefined()
+	expect(every).toBeDefined()
+	expect(hasAll).toBeDefined()
+	expect(isInClosedInterval).toBeDefined()
+	expect(isInLeftClosedInterval).toBeDefined()
+	expect(isInOpenInterval).toBeDefined()
+	expect(isInRange).toBeDefined()
+	expect(isInRightClosedInterval).toBeDefined()
+	expect(none).toBeDefined()
+	expect(some).toBeDefined()
+	expect(startsWith).toBeDefined()
 })
