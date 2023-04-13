1aed0f7da70ccb7eb2edd94fad95250854d8ba33
unional <homawong@gmail.com>
2023-04-12 23:19:16 -0700

    feat: exports helpers from satisfier

--- ts/index.ts
+++ ts/index.ts
@@ -1,7 +1,23 @@
 import { assertron } from './assertron/index.js'
 
+export {
+	anything,
+	has,
+	every,
+	hasAll,
+	isInClosedInterval,
+	isInLeftClosedInterval,
+	isInOpenInterval,
+	isInRange,
+	isInRightClosedInterval,
+	isTypeOf,
+	none,
+	some,
+	startsWith
+} from 'satisfier'
 export * from './assert-order/index.js'
 export * from './assertron/index.js'
 export * from './errors.js'
-export default assertron
 export { assertron as a }
+
+export default assertron
