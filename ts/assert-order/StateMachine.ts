bd842bd868d10992e939f06a50cd430c85c617a7
Homa Wong <homawong@gmail.com>
2023-04-17 16:19:16 -0700

    fix: replace process with global['process']
    This fix loading assertron on browser.

--- ts/assert-order/StateMachine.ts
+++ ts/assert-order/StateMachine.ts
@@ -3,14 +3,14 @@ import type { State } from './types.js'
 
 let timeTracker: { start(): void; taken(): number }
 
-if (process && typeof process.hrtime === 'function') {
+if (global['process'] && typeof global['process'].hrtime === 'function') {
 	let tick: [number, number]
 	timeTracker = {
 		start() {
-			tick = process.hrtime()
+			tick = global['process'].hrtime()
 		},
 		taken() {
-			const [second, nanoSecond] = process.hrtime(tick)
+			const [second, nanoSecond] = global['process'].hrtime(tick)
 			return second * 1000 + nanoSecond / 1e6
 		}
 	}
